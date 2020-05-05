6c2332d095df9ad68fc769fc3070684300be830b
YYYha <1312555615@qq.com>
2020-05-05 16:57:23 +0800

    vue-shop

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # vue-shop
+## 介绍
+这是一个基于vue-cli搭建的商城后台的管理系统，主要使用的技术栈有vue.js、axios、element-ui；通过前后端分离，使用axios与服务器进行数据交互，实现用户管理、用户权限设置、商品管理、订单管理等功能。
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -6,11 +6,24 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 引入字体样式表
 import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
+
 // 引入 axios
 import axios from 'axios'
+// 导入 nprogress 包
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+// 在 require 拦截器中展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem("token")
+  // 最后必须返回 config
+  return config
+})
+
+axios.interceptors.response.use(config =>{
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
@@ -40,6 +53,9 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+
+
+
 
 Vue.use(VueQuillEditor)
 
